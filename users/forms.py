694fadc24fb0ebbe169a44615db7e47d771c5442
johnbenedictyan <johnyfx00@gmail.com>
2019-08-31 09:08:04 +0000

    Added in the django crispy form styling for the login as well as the registration forms. Added in the html for the login and registration pages. Edited the layout.html to add in more jinja blocks.

--- users/forms.py
+++ users/forms.py
@@ -3,11 +3,57 @@ from .models import UserAccount
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import auth
 from pyuploadcare.dj.forms import FileWidget, ImageField
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column, HTML, Div
 
 class LoginForm(forms.Form):
     username = forms.CharField(required=True)
     password = forms.CharField(required=True, widget=forms.PasswordInput)
     
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column(
+                    Div(
+                        HTML(
+                            """
+                            <h3>
+                                Sign In
+                            </h3>
+                            """
+                        )
+                    )
+                )
+            ),
+            Row(
+                Column(
+                    'username', 
+                    css_class='form-group col-12'
+                ),
+                css_class='form-row'
+            ),
+            Row(
+                Column(
+                    'password', 
+                    css_class='form-group col-12'
+                ),
+                css_class='form-row'
+            ),
+            Row(
+                Column(
+                    Submit(
+                        'submit', 
+                        'Sign In', 
+                        css_class="btn btn-primary w-50"
+                    ),
+                    css_class='form-group col-12 text-center'
+                ),
+                css_class='form-row'
+            ),
+        )
+        
     def clean_username(self):
         input_username = self.cleaned_data.get('username')
         if UserAccount.objects.filter(username=input_username).count() == 0:
@@ -31,6 +77,76 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = UserAccount
         fields = ('username','first_name','last_name','email','password1','password2','profile_picture')
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column(
+                    Div(
+                        HTML(
+                            """
+                            <h3>
+                                Register for an Account
+                            </h3>
+                            """
+                        )
+                    )
+                )
+            ),
+            Row(
+                Column(
+                    'profile_picture', 
+                    css_class='form-group col-md-6'
+                ),
+                Column(
+                    'username', 
+                    css_class='form-group col-md-6'
+                ),
+                css_class='form-row'
+            ),
+            Row(
+                Column(
+                    'email', 
+                    css_class='form-group col-12'
+                ),
+                css_class='form-row'
+            ),
+            Row(
+                Column(
+                    'first_name', 
+                    css_class='form-group col-md-6'
+                ),
+                Column(
+                    'last_name', 
+                    css_class='form-group col-md-6'
+                ),
+                css_class='form-row'
+            ),
+            Row(
+                Column(
+                    'password1', 
+                    css_class='form-group col-md-6'
+                ),
+                Column(
+                    'password2', 
+                    css_class='form-group col-md-6'
+                ),
+                css_class='form-row'
+            ),
+            Row(
+                Column(
+                    Submit(
+                        'submit', 
+                        'Register', 
+                        css_class="btn btn-primary w-50"
+                    ),
+                    css_class='form-group col-12 text-center'
+                ),
+                css_class='form-row'
+            ),
+        )
         
     def clean_username(self):
         requested_username = self.cleaned_data.get('username')
