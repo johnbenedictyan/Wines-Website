774e2bae60932464fd6b732d87e1137107a480b8
johnbenedictyan <johnyfx00@gmail.com>
2019-09-02 13:34:03 +0000

    Added the models for both the products as well as the orders.

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from products.models import product as product 
 # Create your models here.
+class order():
+    ordered_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name='orders')
+    stripe_token = models.CharField(max_length=1000, blank=False)
+    date_of_purchase = models.DateTimeField(blank=False)
+    payment_recieved = models.BooleanField(blank=False,default=False)
+    product = models.ManyToManyField(product, related_name="order")
+    
+    def __str__(self):
+        return "Order - " + str(self.id)
+    
--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class product():
+    name = models.CharField(blank=False,max_length = 255)
+    description = models.TextField(blank=False)
+    price = models.FloatField(blank=False,default=0.0)
+    quantity_in_stock = models.IntegerField(blank=False,default=0)
+    
+    def __str__(self):
+        return self.name    
+    
