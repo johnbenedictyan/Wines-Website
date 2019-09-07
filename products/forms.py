a1928ebcb4510eda729c882acdbb30e35fe85fcc
johnbenedictyan <johnyfx00@gmail.com>
2019-09-07 06:07:28 +0000

    Added in the product creation form as well as updated the product model to have more fields.

--- products/forms.py
+++ products/forms.py
@@ -1,36 +1,56 @@
 from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Row, Column
-
 from pyuploadcare.dj.forms import FileWidget, ImageField
 from .models import Product
-class ListingForm(forms.ModelForm):
+
+
+class ProductForm(forms.ModelForm):
     listing_photo = ImageField(widget=FileWidget(attrs={
-        'data-public-key':'c1c0ea35a4b3421770fa',
-        'data-images-only':'True',
-        'data-preview-step':'True',
+        'data-public-key': 'c1c0ea35a4b3421770fa',
+        'data-images-only': 'True',
+        'data-preview-step': 'True',
     }))
+
     class Meta:
         model = Product
-        fields = ('name','description','price','used','location','listing_photo','categories')
-        exclude = ['seller']
-        
-        
+        fields = (
+            'name',
+            'description',
+            'price',
+            'product_picture',
+            'region',
+            'aroma',
+            'body'
+        )
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
             Row(
-                Column('name', css_class='form-group col-md-6 mb-0'),
-                Column('price', css_class='form-group col-md-6 mb-0'),
+                Column(
+                    'name',
+                    css_class='form-group col-md-6 mb-0'
+                ),
+                Column(
+                    'price',
+                    css_class='form-group col-md-6 mb-0'
+                ),
                 css_class='form-row'
             ),
-            'description',
-            'location',
             Row(
-                Column('categories', css_class='form-group col-md-6 mb-0 categories_form_custom_css'),
-                Column('listing_photo', css_class='form-group col-md-4 mb-0'),
-                Column('used', css_class='form-group col-md-2 mb-0'),
+                'description',
+                css_class='form-row'
+            ),
+            Row(
+                Column('region', css_class='form-group'),
+                Column('aroma', css_class='form-group'),
+                Column('body', css_class='form-group'),
+                css_class='form-row'
+            ),
+            Row(
+                'product_picture',
                 css_class='form-row'
             ),
             Submit('submit', 'Create Listing', css_class="btn essence-btn")
--- products/models.py
+++ products/models.py
@@ -1,12 +1,88 @@
 from django.db import models
-
+from pyuploadcare.dj.models import ImageField
 # Create your models here.
+
+
 class Product():
-    name = models.CharField(blank=False,max_length = 255)
-    description = models.TextField(blank=False)
-    price = models.FloatField(blank=False,default=0.0)
-    quantity_in_stock = models.IntegerField(blank=False,default=0)
-    
+    FRANCE = "FR"
+    ITALY = "IT"
+    USA = "US"
+    SPAIN = "ES"
+    PORTUGUAL = "PT"
+    ARGENTINA = "AR"
+    AUSTRALIA = "AU"
+    NEW_ZEALAND = "NZ"
+    UNITED_KINGDOM = "UK"
+
+    LIGHT_BODIED = "LB"
+    MEDIUM_BODIED = "MB"
+    FULL_BODIED = "FB"
+
+    REGION_CHOICES = [
+        (FRANCE, "France"),
+        (ITALY, "Italy"),
+        (USA, "USA"),
+        (SPAIN, "Spain"),
+        (PORTUGUAL, "Portugal"),
+        (ARGENTINA, "Argentina"),
+        (AUSTRALIA, "Australia"),
+        (NEW_ZEALAND, "New Zealand"),
+        (UNITED_KINGDOM, "United Kingdom")
+    ]
+
+    BODY_CHOICES = [
+        (LIGHT_BODIED, "Light-Bodied"),
+        (MEDIUM_BODIED, "Medium-Bodied"),
+        (FULL_BODIED, "Full-Bodied")
+    ]
+
+    AROMA_CHOICES = [
+        "Fruits",
+        "Herbs",
+        "Flowers",
+        "Earth",
+        "Grass",
+        "Tobacco",
+        "Butterscotch",
+        "Toast",
+        "Vanilla",
+        "Mocha",
+        "Chocolate"
+    ]
+
+    name = models.CharField(
+        blank=False,
+        max_length=255
+    )
+    description = models.TextField(
+        blank=False
+    )
+    price = models.FloatField(
+        blank=False,
+        default=0.0
+    )
+    quantity_in_stock = models.IntegerField(
+        blank=False,
+        default=0
+    )
+    product_picture = ImageField(
+        blank=False
+    )
+    region = models.CharField(
+        blank=False,
+        max_length=1,
+        choices=REGION_CHOICES,
+    )
+    aroma = models.CharField(
+        blank=False,
+        max_length=1,
+        choices=BODY_CHOICES,
+    )
+    body = models.CharField(
+        blank=False,
+        max_length=1,
+        choices=AROMA_CHOICES,
+    )
+
     def __str__(self):
-        return self.name    
-    
+        return self.name
