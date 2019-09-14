15e950d8a085b1761833ef1b607b7f1a937058ad
johnbenedictyan <johnyfx00@gmail.com>
2019-09-14 05:16:47 +0000

    Added the initial views of the product application.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def shop(request):
+    all_products = None
+    return render(
+        request,
+        "shop.html",
+        {
+            "all_products":all_products
+        })
+        
+def individual_product(request):
+    single_product = None
+    return render(
+        request,
+        "shop-single.html",
+        {
+            "single_product":single_product
+        })
