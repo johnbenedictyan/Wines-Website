53d6673887f11037a2d7e7c461a99df8d68e5b1c
johnbenedictyan <johnyfx00@gmail.com>
2019-09-14 07:17:54 +0000

    Updates the views.py file for the product application to render the product listing creation form as well as the inventory page.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ProductForm
 
 # Create your views here.
 def shop(request):
@@ -18,3 +21,41 @@ def individual_product(request):
         {
             "single_product":single_product
         })
+
+@login_required
+def inventory(request):
+    all_products = None
+    return render(
+        request,
+        "inventory.html",
+        {
+            "all_products":all_products
+        })
+        
+@login_required
+def product_creator(request):
+    if request.method == "GET":
+        product_form = ProductForm()
+        return render(
+            request,
+            "product-form.html",
+            {
+                "product_form":product_form
+            })
+    else:
+        dirty_product_form = ProductForm(request.POST)
+        if dirty_product_form.is_valid():
+            dirty_product_form.save()
+            return redirect(None)
+        else:
+            messages.error(
+                request,
+                "We were unable to create a listing for this product."
+                )
+            return render(
+                request,
+                "product-form.html",
+                {
+                    "product_form":dirty_product_form
+                })
+            
