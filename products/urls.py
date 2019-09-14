17740411fe7c7f9710d713fc8f9b80c8e8dce2ed
johnbenedictyan <johnyfx00@gmail.com>
2019-09-14 05:14:35 +0000

    Added the name attribute the the url patterns.

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
 from django.urls import path
+from .views import shop,individual_product
 
 urlpatterns = [
+    path('', shop, name="shop"),
+    path('product/<int:product_number>/', individual_product, name="shop")
 ]
