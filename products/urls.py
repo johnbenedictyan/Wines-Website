98b0f59c47485953ba1a840704d999b788bea0e1
johnbenedictyan <johnyfx00@gmail.com>
2019-09-14 08:02:01 +0000

    Added the url patterns of the product creation page and the product details updation.

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import shop,individual_product
+from .views import shop,individual_product,product_creator,product_update
 
 urlpatterns = [
     path('', shop, name="shop"),
-    path('product/<int:product_number>/', individual_product, name="shop")
+    path('product/<int:product_number>/', individual_product, name="individual_product"),
+    path('product/update/<int:product_number>/', product_update, name="product_update"),
+    path('product/create/', product_creator, name="product_creator")
 ]
