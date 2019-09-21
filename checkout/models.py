8bed1518af40dbe0f7cc4a05b0926ad965fdfd69
johnbenedictyan <johnyfx00@gmail.com>
2019-09-21 06:21:46 +0000

    Added a max length option to all of the Charfield as it was a requirement.

--- checkout/models.py
+++ checkout/models.py
@@ -13,45 +13,54 @@ class Customer_Details(models.Model):
     
     first_name = models.CharField(
         verbose_name="First Name",
+        max_length=50,
         blank=False
     )
     
     last_name = models.CharField(
         verbose_name="Last Name",
+        max_length=50,
         blank=False
     )
     
     address_1 = models.CharField(
         verbose_name="Address",
+        max_length=50,
         blank=False
     )
         
     address_2 = models.CharField(
+        max_length=50,
         blank=False
     )
         
     state_or_country = models.CharField(
         verbose_name="State / Country",
+        max_length=50,
         blank=False
     )
     
     postal_code_or_zip = models.CharField(
         verbose_name="Postal Code / Zip",
+        max_length=50,
         blank=False
     )
     
     email = models.CharField(
         verbose_name="Email Address",
+        max_length=50,
         blank=False
     )
     
     phone = models.CharField(
         verbose_name="Phone Number",
+        max_length=50,
         blank=False
     )
 
     account_password = models.CharField(
         verbose_name="Account Password",
+        max_length=50,
         blank=True
     )
 
@@ -62,27 +71,33 @@ class Customer_Details(models.Model):
         
     alt_address_1 = models.CharField(
         verbose_name="Address",
+        max_length=50,
         blank=True
     )
     alt_address_2 = models.CharField(
+        max_length=50,
         blank=True
     )
     alt_state_or_country = models.CharField(
         verbose_name="State / Country",
+        max_length=50,
         blank=True
     )
     alt_postal_code_or_zip = models.CharField(
         verbose_name="Postal Code / Zip",
+        max_length=50,
         blank=True
     )
 
     coupon_code = models.CharField(
         verbose_name="Enter your coupon code if you have one",
+        max_length=50,
         blank=True
     )
 
     order_notes = models.CharField(
         verbose_name="Order Notes",
+        max_length=200,
         blank=True
     )
     
