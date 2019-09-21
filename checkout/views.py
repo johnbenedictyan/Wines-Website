e33e7f268a47aed38b1c68afa5bcdd7b5917550a
johnbenedictyan <johnyfx00@gmail.com>
2019-09-21 06:22:20 +0000

    Changed the name of the customer details form and as such had to change the import statement.

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import DetailForm
+from .forms import CustomerDetailForm
 
 # Create your views here.
 def checkout(request):
