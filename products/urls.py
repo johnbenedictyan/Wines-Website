5724b0886e5142b25815b5082dbb5a25fb20e9a0
johnbenedictyan <johnyfx00@gmail.com>
2019-10-04 13:36:19 +0000

    Added more urls.

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import shop,individual_product,product_creator,product_update
+from .views import shop,individual_product,product_creator,product_update,delete_product,inventory
 
 urlpatterns = [
     path('', shop, name="shop"),
-    path('product/<int:product_number>/', individual_product, name="individual_product"),
-    path('product/update/<int:product_number>/', product_update, name="product_update"),
-    path('product/create/', product_creator, name="product_creator")
+    path('products/<int:product_number>/', individual_product, name="individual_product"),
+    path('products/update/<int:product_number>/', product_update, name="product_update"),
+    path('products/create/', product_creator, name="product_creator"),
+    path('products/delete/<int:product_number>/', product_creator, name="product_creator"),
+    path('products/inventory/', product_creator, name="product_creator"),
 ]
