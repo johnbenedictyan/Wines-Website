33158c2b451d19a478a975462d7c615fa425b8e9
johnbenedictyan <johnyfx00@gmail.com>
2019-10-12 00:41:27 +0000

    Added in a url path of the wine collection page. Edited the existing urls that didnt match their corresponding views functions.

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import shop,individual_product,product_creator,product_update,delete_product,inventory
+from .views import shop,individual_product,product_creator,product_update,delete_product,inventory,wine_collection
 
 urlpatterns = [
     path('', shop, name="shop"),
+    path('collection/', wine_collection, name="wine_collection"),
     path('products/<int:product_number>/', individual_product, name="individual_product"),
     path('products/update/<int:product_number>/', product_update, name="product_update"),
     path('products/create/', product_creator, name="product_creator"),
-    path('products/delete/<int:product_number>/', product_creator, name="product_creator"),
-    path('products/inventory/', product_creator, name="product_creator"),
+    path('products/delete/<int:product_number>/', delete_product, name="delete_product"),
+    path('products/inventory/', inventory, name="inventory"),
 ]
