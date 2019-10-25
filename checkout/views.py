f9fd3f03c4425875309cc501a3026f7576d2a16b
johnbenedictyan <johnyfx00@gmail.com>
2019-10-25 04:04:23 +0000

    Moved the cart class into the global frame of the views.py so that all of the cart view functions can access it.

--- checkout/views.py
+++ checkout/views.py
@@ -25,41 +25,43 @@ def checkout(request):
                 "We are unable to accept your details."
                 )
             
-def view_cart(request):
-    class cart:
-        def __init__(self):
-            self.cart_items = []
-        
-        def add_item_to_cart(self,cart_item):
-            cart_item.total_price = cart_item.price * cart_item.quantity
-            self.cart_items.append(cart_item)
-        
-        def edit_item_quantity(self,product_number,new_quantity):
-            found = False
-            for idx,item in enumerate(self.cart_items):
-                if item.product_number == product_number:
-                    item.quantity = new_quantity
-                    found = True
-                    found_product_number = item.product_number
-                    
-            if found is False:
-                return None
-            else:
-                return found_product_number
-        
-        def delete_item_quantity(self,product_number):
-            found = False
-            for idx,item in enumerate(self.cart_items):
-                if item.product_number == product_number:
-                    found = True
-                    found_product_position = idx
-                    
-            if found is False:
-                return None
-            else:
-                self.cart_items.pop(found_product_position)
-                return True
+# Cart Class used by all of the cart view functions
+class cart:
+    def __init__(self):
+        self.cart_items = []
     
+    def add_item_to_cart(self,cart_item):
+        cart_item.total_price = cart_item.price * cart_item.quantity
+        self.cart_items.append(cart_item)
+    
+    def edit_item_quantity(self,product_number,new_quantity):
+        found = False
+        for idx,item in enumerate(self.cart_items):
+            if item.product_number == product_number:
+                item.quantity = new_quantity
+                found = True
+                found_product_number = item.product_number
+                
+        if found is False:
+            return None
+        else:
+            return found_product_number
+    
+    def delete_item_quantity(self,product_number):
+        found = False
+        for idx,item in enumerate(self.cart_items):
+            if item.product_number == product_number:
+                found = True
+                found_product_position = idx
+                
+        if found is False:
+            return None
+        else:
+            self.cart_items.pop(found_product_position)
+            return True
+    
+    
+def view_cart(request):
     user_cart = request.session.get('user_cart', cart())
     print(user_cart)
     return render(
