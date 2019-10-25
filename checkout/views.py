e057b7be9217e7e0a5e3551155d463a7f0fca69b
johnbenedictyan <johnyfx00@gmail.com>
2019-10-25 03:41:28 +0000

    Changed the car from model based to session based. Added a cart constructor with all of its necessary functions. Added a add view cart view function so that the user can view his/her cart.

--- checkout/views.py
+++ checkout/views.py
@@ -25,11 +25,53 @@ def checkout(request):
                 "We are unable to accept your details."
                 )
             
-def cart(request):
-    selected_cart = None
+def view_cart(request):
+    class cart:
+        def __init__(self):
+            self.cart_items = []
+        
+        def add_item_to_cart(self,cart_item):
+            cart_item.total_price = cart_item.price * cart_item.quantity
+            self.cart_items.append(cart_item)
+        
+        def edit_item_quantity(self,product_number,new_quantity):
+            found = False
+            for idx,item in enumerate(self.cart_items):
+                if item.product_number == product_number:
+                    item.quantity = new_quantity
+                    found = True
+                    found_product_number = item.product_number
+                    
+            if found is False:
+                return None
+            else:
+                return found_product_number
+        
+        def delete_item_quantity(self,product_number):
+            found = False
+            for idx,item in enumerate(self.cart_items):
+                if item.product_number == product_number:
+                    found = True
+                    found_product_position = idx
+                    
+            if found is False:
+                return None
+            else:
+                self.cart_items.pop(found_product_position)
+                return True
+    
+    user_cart = request.session.get('user_cart', cart())
+    print(user_cart)
     return render(
         request,
         "cart.html",
         {
-            "selected_cart":selected_cart
+            "user_cart":user_cart
         })
+        
+def add_to_cart(request,product_number):
+    return None
+
+def change_cart(request,product_number,new_number):
+    return None
+    
