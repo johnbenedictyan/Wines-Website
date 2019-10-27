d1a7a3b4a3bfb28d755decf9a4609fce185d31f3
johnbenedictyan <johnyfx00@gmail.com>
2019-10-27 17:26:38 +0000

    Added the url route for the coupon check view function.

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import view_cart,add_to_cart,edit_cart,checkout,delete_from_cart,clear_cart
+from .views import view_cart,add_to_cart,edit_cart,checkout,delete_from_cart,clear_cart,coupon_check
 urlpatterns = [
     path('cart/', view_cart, name="view_cart"),
     path('cart/add/<int:product_number>/<int:quantity>/', add_to_cart, name="add_to_cart"),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('cart/delete/<int:product_number>/', delete_from_cart, name="delete_from_cart"),
     path('cart/clear/', clear_cart, name="clear_cart"),
     path('checkout/', checkout, name="checkout"),
+    path('coupon/', coupon_check, name="coupon_check"),
 ]
