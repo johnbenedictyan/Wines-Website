4b63af407203108280af4f573cb8638d29499249
johnbenedictyan <johnyfx00@gmail.com>
2019-11-10 09:47:33 +0000

    Registered the object product intermediary object.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Order,Coupon,Customer_Detail
+from .models import Order,Coupon,Customer_Detail,Order_Product_Intermediary
 # Register your models here.
 admin.site.register(Order)
 admin.site.register(Coupon)
 admin.site.register(Customer_Detail)
+admin.site.register(Order_Product_Intermediary)
