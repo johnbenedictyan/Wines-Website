653eb64996525020468382e24ab0e8dee1f1b984
johnbenedictyan <johnyfx00@gmail.com>
2019-11-23 08:03:10 +0000

    Added the whitenoise middleware and reformatted the static and media directories variables.

--- project4/settings.py
+++ project4/settings.py
@@ -79,6 +79,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'project4.urls'
@@ -148,23 +149,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS=[
+STATICFILES_DIRS = (
     os.path.join(
-        BASE_DIR,
-        "static"
+        BASE_DIR, 'static'
+        ),
     )
-]
-
 STATIC_ROOT = os.path.join(
-    BASE_DIR,
+    BASE_DIR, 
     'staticfiles'
-)
-    
-MEDIA_URL = "/media/"
+    )
 
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(
     BASE_DIR,
     'media'
