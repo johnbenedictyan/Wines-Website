f3aa11e24b0b783f2d38a1d8983df012ec4d927a
johnbenedictyan <johnyfx00@gmail.com>
2019-11-24 10:42:54 +0000

    Added the tests for a invalid email address and phone number.

--- website/tests.py
+++ website/tests.py
@@ -131,3 +131,45 @@ class WebsiteFormTest(TestCase):
             'message',
             'This field is required.'
             )
+            
+    def testInvalidPhoneNumberErrorMessage(self):
+        test_form_data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'phone_number': 'This is not a phone number.',
+            'email_address': 'testuser@testuseremail.com',
+            'message': 'Hi I would like to say that I am a test user.',
+        }
+        test_form = ContactForm(
+            data=test_form_data
+            )
+        self.assertFalse(test_form.is_valid())
+        response = self.client.post('/contact-us/', test_form_data)
+        self.assertFormError(
+            response,
+            'contact_form',
+            'phone_number',
+            'Enter a whole number.'
+            )
+    
+    def testInvalidEmailAddressErrorMessage(self):
+        test_form_data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'phone_number': 12345678,
+            'email_address': 'This is not a valid email address.',
+            'message': 'Hi I would like to say that I am a test user.',
+        }
+        test_form = ContactForm(
+            data=test_form_data
+            )
+        self.assertFalse(test_form.is_valid())
+        response = self.client.post('/contact-us/', test_form_data)
+        self.assertFormError(
+            response,
+            'contact_form',
+            'email_address',
+            'Enter a valid email address.'
+            ) 
+    
+    
