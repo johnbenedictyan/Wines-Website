71ca49ed06537079d34456365cb91ef9b9662d8c
johnbenedictyan <johnyfx00@gmail.com>
2019-11-24 15:02:32 +0000

    Added in the user account tests for valid and invalid logins.

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .models import UserAccount
+from .forms import RegisterForm
+from django.contrib import auth
+
 # Users Test Cases
 # Create your tests here.
 DEFAULT_IMAGE_UUID = "0662e7f0-e44d-4f4b-8482-715f396f5fb0"
@@ -88,3 +91,46 @@ class UserAccountUrlTest(TestCase):
         response = self.client.get('/users/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'account.html')
+        
+class UserAccountFormTest(TestCase):
+    def setUp(self):
+        ta = create_account()
+        ta.set_password('password123')
+        ta.save()
+        self.user = ta
+        
+    def testValidLogin(self):
+        test_form_data = {
+            'username': 'penguinrider',
+            'password': 'password123',
+        }
+        response = self.client.post(
+            '/users/log-in/',
+            test_form_data
+            )
+        self.assertRedirects(
+            response,
+            '/',
+            status_code=302,
+            target_status_code=200
+            )
+        self.assertIn('_auth_user_id', self.client.session)
+
+    def testInvalidLogin(self):
+        test_form_data = {
+            'username': 'penguinrider',
+            'password': 'password1234',
+        }
+        response = self.client.post(
+            '/users/log-in/',
+            test_form_data
+            )
+        self.assertRedirects(
+            response,
+            '/users/log-in/',
+            status_code=302,
+            target_status_code=200
+            )
+        self.assertNotIn('_auth_user_id', self.client.session)
+
+    
