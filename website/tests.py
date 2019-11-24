0f4dfd59e910554051778ff5f43c421fdcc473dd
johnbenedictyan <johnyfx00@gmail.com>
2019-11-24 09:36:55 +0000

    Added in the url test for the website app.

--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 # Website Test Cases
 # Create your tests here.
+class WebsiteURLTest(TestCase):
+    def testCanLoadMainPage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+    
+    def testCanLoadContactPage(self):
+        response = self.client.get('/contact-us/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact.html')
+    
+    def testCanLoadAboutUsPage(self):
+        response = self.client.get('/about-us/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about-us.html')
+    
+    def testCannotLoadNonExistentPage(self):
+        response = self.client.get('/flyingpenguin')
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed(response, '404.html')
+    
