e62fdf5994adf983ca288b19596705cfbb19cfdc
johnbenedictyan <johnyfx00@gmail.com>
2019-11-24 10:23:10 +0000

    Added individual tests for each of the fields in the contact us form being left blank and that the form would display the correct error message.

--- website/tests.py
+++ website/tests.py
@@ -36,3 +36,98 @@ class WebsiteFormTest(TestCase):
             data=test_form_data
             )
         self.assertTrue(test_form.is_valid())
+    
+    def testMissingFirstNameErrorMessage(self):
+        test_form_data = {
+            'last_name': 'User',
+            'phone_number': 12345678,
+            'email_address': 'testuser@testuseremail.com',
+            'message': 'Hi I would like to say that I am a test user.',
+        }
+        test_form = ContactForm(
+            data=test_form_data
+            )
+        self.assertFalse(test_form.is_valid())
+        response = self.client.post('/contact-us/', test_form_data)
+        self.assertFormError(
+            response,
+            'contact_form',
+            'first_name',
+            'This field is required.'
+            )
+        
+    def testMissingLastNameErrorMessage(self):
+        test_form_data = {
+            'first_name': 'Test',
+            'phone_number': 12345678,
+            'email_address': 'testuser@testuseremail.com',
+            'message': 'Hi I would like to say that I am a test user.',
+        }
+        test_form = ContactForm(
+            data=test_form_data
+            )
+        self.assertFalse(test_form.is_valid())
+        response = self.client.post('/contact-us/', test_form_data)
+        self.assertFormError(
+            response,
+            'contact_form',
+            'last_name',
+            'This field is required.'
+            )
+    
+    def testMissingPhoneNumberErrorMessage(self):
+        test_form_data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'email_address': 'testuser@testuseremail.com',
+            'message': 'Hi I would like to say that I am a test user.',
+        }
+        test_form = ContactForm(
+            data=test_form_data
+            )
+        self.assertFalse(test_form.is_valid())
+        response = self.client.post('/contact-us/', test_form_data)
+        self.assertFormError(
+            response,
+            'contact_form',
+            'phone_number',
+            'This field is required.'
+            )
+    
+    def testMissingEmailAddressErrorMessage(self):
+        test_form_data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'phone_number': 12345678,
+            'message': 'Hi I would like to say that I am a test user.',
+        }
+        test_form = ContactForm(
+            data=test_form_data
+            )
+        self.assertFalse(test_form.is_valid())
+        response = self.client.post('/contact-us/', test_form_data)
+        self.assertFormError(
+            response,
+            'contact_form',
+            'email_address',
+            'This field is required.'
+            )
+    
+    def testMissingMessageErrorMessage(self):
+        test_form_data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'phone_number': 12345678,
+            'email_address': 'testuser@testuseremail.com',
+        }
+        test_form = ContactForm(
+            data=test_form_data
+            )
+        self.assertFalse(test_form.is_valid())
+        response = self.client.post('/contact-us/', test_form_data)
+        self.assertFormError(
+            response,
+            'contact_form',
+            'message',
+            'This field is required.'
+            )
