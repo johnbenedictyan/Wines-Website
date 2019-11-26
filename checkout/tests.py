c1339940e8807afdfe16f4c0e7c2241b6838a385
johnbenedictyan <johnyfx00@gmail.com>
2019-11-26 13:06:42 +0000

    Initialised the checkout test with helper functions.

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from users.models import UserAccount
+from product.models import Product
 # Checkout Test Cases
 # Create your tests here.
+DEFAULT_IMAGE_UUID = "0662e7f0-e44d-4f4b-8482-715f396f5fb0"
+
+def create_account():
+    ta = UserAccount(
+        username="penguinrider",
+        password="password123",
+        email="asd@asd.com",
+        first_name="penguin",
+        last_name="rider",
+        bio="Hi im a penguinrider",
+        profile_picture=DEFAULT_IMAGE_UUID
+        )
+    ta.save()
+    return ta
+
+def create_test_product(seller_id):
+    tp = Product(
+            name="Generic Wine",
+            year=2013,
+            description="This is a bottle of Generic Wine",
+            price=53.99,
+            quantity_in_stock=100,
+            product_picture=DEFAULT_IMAGE_UUID,
+            region="FRANCE",
+            nodes="Fruits",
+            body="Light",
+            seller_id=seller_id,
+            views=0
+            )
+    tp.save()
+    return tp
+    
