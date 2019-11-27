9d66d9acc7223f6b4dfbb487fa91cf0eaffbf87a
johnbenedictyan <johnyfx00@gmail.com>
2019-11-27 13:49:58 +0000

    Added a test for the act of editing an item's quantity when the item does not exist in the current cart.

--- checkout/tests.py
+++ checkout/tests.py
@@ -236,3 +236,37 @@ class CheckoutCartEditFunctionTest(TestCase):
         self.assertEqual(user_cart['cart_items'][0]['quantity'], 3)
         self.assertEqual(user_cart['cart_items'][1]['product_number'], 2)
         self.assertEqual(user_cart['cart_items'][1]['quantity'], 1)
+        
+    def testCannotEditItemInCartSelectedItemNotInCart(self):
+        self.client.login(
+            username='penguinrider',
+            password='password123'
+            )
+            
+        response = self.client.get('/checkout/cart/add/1/1/')
+        
+        user_cart = self.client.session['user_cart']
+        
+        self.assertEqual(user_cart['cart_items'][0]['product_number'], 1)
+        self.assertEqual(user_cart['cart_items'][0]['quantity'], 1)
+        
+        test_form_data = {
+            'product-number': ['2'],
+            'item-quantity': ['3'],
+            'coupon-applied': ['no-coupon'],
+            'chargable-percentage': ['1']
+        }
+        
+        response = self.client.post('/checkout/cart/edit/', test_form_data)
+        
+        self.assertRedirects(
+            response,
+            '/checkout/cart/',
+            status_code=302,
+            target_status_code=200
+            )    
+            
+        user_cart = self.client.session['user_cart']
+        
+        self.assertEqual(user_cart['cart_items'][0]['product_number'], 1)
+        self.assertEqual(user_cart['cart_items'][0]['quantity'], 1)
