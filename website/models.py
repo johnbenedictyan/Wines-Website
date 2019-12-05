b808d8d8075dd0630473fe1b96bb701eb4dd3171
johnbenedictyan <johnyfx00@gmail.com>
2019-12-05 14:30:21 +0000

    Created the Contact model for the contact form modelform.

--- website/models.py
+++ website/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(
+        blank=False,
+        max_length=30
+    )
+    last_name = models.CharField(
+        blank=False,
+        max_length=30
+    )
+    phone_number = models.IntegerField(
+        blank=False,
+    )
+    email_address = models.EmailField(
+        blank=False,
+    )
+    message = models.TextField(
+        blank=False
+    )
