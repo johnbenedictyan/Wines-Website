2608b1c975bb59d79c184f5f0654a7050ddcb8fe
johnbenedictyan <johnyfx00@gmail.com>
2019-12-21 03:38:36 +0000

    Added a new model named Blog

--- website/models.py
+++ website/models.py
@@ -5,17 +5,26 @@ class Contact(models.Model):
     first_name = models.CharField(
         blank=False,
         max_length=30
-    )
+        )
     last_name = models.CharField(
         blank=False,
         max_length=30
-    )
+        )
     phone_number = models.IntegerField(
         blank=False,
-    )
+        )
     email_address = models.EmailField(
         blank=False,
-    )
+        )
     message = models.TextField(
         blank=False
-    )
+        )
+
+class Blog(models.Model):
+    headline = models.CharField(
+        blank=False,
+        max_length=100
+        )
+    body = models.TextField(
+        blank=False
+        )
