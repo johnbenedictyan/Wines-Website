6f586031c3a8add9467ec435789dbb856a97706a
johnbenedictyan <johnyfx00@gmail.com>
2019-12-26 06:13:40 +0000

    Added the urls for the blog view functions.

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import main_page,about_us,contact_us
+from .views import main_page,about_us,contact_us,blog_creator,bloghub,single_blog
 
 urlpatterns = [
     path('', main_page, name="main_page"),
     path('about-us/', about_us, name="about_us"),
     path('contact-us/', contact_us, name="contact_us"),
+    path('blog/<int:blog_number>/', single_blog, name="single_blog"),
+    path('blog/create/', blog_creator, name="blog_creator"),
+    path('blog/', bloghub, name="bloghub"),
 ]
